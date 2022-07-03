6b142aea9f598cabe91e0b589ec26bdb83ca82b2
kesavanandvs <108583767+kesavanandvs@users.noreply.github.com>
2022-07-03 10:34:45 +0530

    Update settings.py

--- back/settings.py
+++ back/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't($8pswz-k5o!fl=$#@^6zo-$l0gk)ps6+1_h-b6cu8d*vmsm6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://127.0.0.1:8000','']
 
 
 # Application definition
@@ -120,9 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]  may be used later
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
